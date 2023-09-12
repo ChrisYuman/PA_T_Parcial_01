5d3dbe26b4fe9c58c5626fe8421cda34f3d0969b
Daniel Mejia <rdmejiaa@gmail.com>
2023-09-11 19:16:44 -0600

    Ejercicio 1 ResueltoAdd note to readme

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -10,5 +10,28 @@ char Ejercicio01::lower(char c)
 
 bool Ejercicio01::isPalindrome(const char* phrase, int length)
 {
-    return false;
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end)
+    {
+        if (!((phrase[start]>= 'a' && phrase[start]<= 'z')||(phrase[start]>='A' && phrase[start]<='Z')|| (phrase[start]>='0' && phrase[start]<= '9')) || phrase[start] == ' ')
+        {
+            start++;
+        }
+        else if (!((phrase[end] >= 'a' && phrase[end] <= 'z') || (phrase[end] >= 'A' && phrase[end] <= 'Z') || (phrase[end] >= '0' && phrase[end] <= '9')) || phrase[end]== ' ')
+        {
+            end--;
+        }
+        else if (lower(phrase[start] | 32) != lower(phrase[end] | 32))
+        {
+            return false;
+        }
+        else
+        {
+            start++;
+            end--;
+        }
+    }
+    return true;
 }
