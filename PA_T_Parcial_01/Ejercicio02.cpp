541e733e31647ada0db8d32c87d031cda37fe858
Alejandro <alejoramvids@gmail.com>
2023-09-12 08:28:12 -0600

    Ejercicio 2, resuelto

--- PA_T_Parcial_01/Ejercicio02.cpp
+++ PA_T_Parcial_01/Ejercicio02.cpp
@@ -2,5 +2,20 @@
 
 int Ejercicio02::rob(int houses[], int n)
 {
-    return 0;
+    if (n == 0)
+    {
+        return 0;
+    }
+    int casanterior = 0;
+    int casaanteanterior = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int casas = casanterior;
+        if(casaanteanterior + houses[i] > casanterior)
+        {
+            casanterior = casaanteanterior + houses[i];
+        }
+        casaanteanterior = casas;
+    }
+    return casanterior;
 }
